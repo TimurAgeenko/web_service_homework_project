dbd12d25d876da2ca197c58166ffdbaaa506894c
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-03-20 02:59:49 +0300

    Added processing POST requests

--- config/catalog/views.py
+++ config/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
@@ -6,4 +7,9 @@ def home_page(request):
 
 
 def contacts_page(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone_number = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, "catalog/contacts.html")
