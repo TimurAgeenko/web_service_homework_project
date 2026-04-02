a5f25a46fc798c925006f640761b2bbfda18c066
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-02 23:04:14 +0300

    Added pagination of products

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from .models import Product, Contacts, Category
 
 
 def home_page(request):
-    products = Product.objects.all()[:6]
-    return render(request, "catalog/home.html", {'products': products})
+    products = Product.objects.all()
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "catalog/home.html", {'page_obj': page_obj})
 
 
 def contacts_page(request):
