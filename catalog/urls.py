91f5b6e450c0a7a319f56f789de05091faeaa681
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-02 22:05:17 +0300

    Added adding_product page

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("home/", views.home_page, name="home"),
     path("contacts/", views.contacts_page, name="contacts"),
     path("products/<int:product_id>/", views.product_page, name="product"),
+    path("adding_product/", views.adding_product_page, name="adding"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contacts
+from .models import Product, Contacts, Category
 
 
 def home_page(request):
@@ -21,3 +21,18 @@ def contacts_page(request):
 def product_page(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, "catalog/product.html", {'product': product})
+
+
+def adding_product_page(request):
+    categories = Category.objects.all()
+    if request.method == "POST":
+        name = request.POST.get("name")
+        category_id = request.POST.get("category")
+        category = Category.objects.get(id=category_id)
+        price = request.POST.get("price")
+        image = request.FILES.get("image")
+        description = request.POST.get("description")
+
+        product = Product.objects.create(name=name, category=category, price=price, image=image, description=description)
+        return render(request, "catalog/adding_product.html", {'product': product})
+    return render(request, "catalog/adding_product.html", {'categories': categories})
