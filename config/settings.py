47da52a9cca5cb3abfab9d648bd8a5bccee6de20
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-04 12:48:10 +0300

    Changed settings.py and base.html files

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # Media files loaded by users
