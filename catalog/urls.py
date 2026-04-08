7faf81b4d9afbdf7723f1338834c03e7046fa5b1
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-08 14:19:30 +0300

    Fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,6 @@ app_name = "catalog"
 urlpatterns = [
     path("home/", ProductListView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("adding_product/", ProductCreateView.as_view(), name="adding"),
 ]
