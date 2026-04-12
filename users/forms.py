bc669e91f43ad3b0ca8f8abb017f46292208bd9e
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-13 01:07:07 +0300

    Added register.html CustomUserCreationForm and RegisterView classes

---
+++ users/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email', 'username', 'first_name', 'last_name', 'phone_number', 'country', 'password1', 'password2')
+
+        labels = {
+            'email': 'Электронная почта',
+            'username': 'Имя пользователя',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'phone_number': 'Номер телефона',
+            'country': 'Страна',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.error_messages = {
+            'password_mismatch': 'Пароли не совпадают. Попробуйте ещё раз.',
+        }
+
+        if 'password1' in self.fields:
+            self.fields['password1'].label = "Пароль"
+
+        if 'password2' in self.fields:
+            self.fields['password2'].label = "Подтвердите пароль"
+
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+            field.help_text = ''
+
+            if field.required:
+                field.label = f"{field.label}*"
