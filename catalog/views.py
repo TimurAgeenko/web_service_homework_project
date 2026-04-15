f0db41c66835a7d48252c29e2007f2179c17b688
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-04-15 14:38:34 +0300

    Added caching for ProductDetailView

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import CreateView, DetailView, FormView, ListView, UpdateView, DeleteView, View
 
@@ -38,6 +40,7 @@ class ContactsView(FormView):
         return super().form_valid(form)
 
 
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
